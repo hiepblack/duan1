792b0aa97acc0b082674e6d950a94cc443a533f2
hiepblack <chuongduong0123@gmail.com>
2022-11-08 21:17:13 +0700

    update2

--- customer/login_user.php
+++ customer/login_user.php
@@ -9,12 +9,12 @@
     if($result){
         if(checkUser($result)){
             $_SESSION['user'] = $result;
-            header("Location:http://localhost/WEB17301/du_an_1/admin/index.php?act=trangchu");
+            header("Location:http://localhost/WEB17301/Du_an_1/admin/index.php?act=trangchu");
         }else{
             $_SESSION['user'] = $result;
-            header("Location:http://localhost/WEB17301/du_an_1/views/index.php?act=trangchu&succes=$username");
+            header("Location:http://localhost/WEB17301/Du_an_1/views/index.php?act=trangchu&succes=$username");
         }
     }else{
-        header("Location:http://localhost/WEB17301/du_an_1/views/index.php?act=dangnhap&error");
+        header("Location:http://localhost/WEB17301/Du_an_1/views/index.php?act=dangnhap&error");
         exit();
 }
