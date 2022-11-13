e9e54678f71b0ff629a6864dbf70951d572dbca2
hiepblack <chuongduong0123@gmail.com>
2022-11-13 22:00:48 +0700

    update4

--- views/sanpham.php
+++ views/sanpham.php
@@ -20,36 +20,40 @@
             <form class="grid grid-cols-4 w-4/5 gap-4 h-10" action="http://localhost/WEB17301/Du_an_1/views/index.php?act=sanpham" method="post">
                 <select class="form-control" name="danhmuc">
                     <option value="0">Danh Mục</option>
-                    <option value="1" <?php echo isset($_POST['danhmuc'])&&$_POST['danhmuc']==1 ?"selected":"";?>>Xe đạp địa hình</option>
-                    <option value="2" <?php echo isset($_POST['danhmuc'])&&$_POST['danhmuc']==2 ?"selected":"";?>>Xe đạp Thời Trang</option>
-                    <option value="3" <?php echo isset($_POST['danhmuc'])&&$_POST['danhmuc']==3 ?"selected":"";?>>Xe đạp thể thao</option>
+                    <option value="1" <?php echo isset($_POST['danhmuc']) && $_POST['danhmuc'] == 1 ? "selected" : ""; ?>>Xe đạp địa hình</option>
+                    <option value="2" <?php echo isset($_POST['danhmuc']) && $_POST['danhmuc'] == 2 ? "selected" : ""; ?>>Xe đạp Thời Trang</option>
+                    <option value="3" <?php echo isset($_POST['danhmuc']) && $_POST['danhmuc'] == 3 ? "selected" : ""; ?>>Xe đạp thể thao</option>
                 </select>
                 <select class="form-control" name="gia">
                     <option value="0">Giá Sản Phẩm</option>
-                    <option value="500000-1000000" <?php echo isset($_POST['gia'])&&$_POST['gia']=="500000-1000000" ?"selected":"";?>>Từ 500.000 - 1.000.000</option>
-                    <option value="1000000-1500000" <?php echo isset($_POST['gia'])&&$_POST['gia']=="1000000-1500000" ?"selected":"";?>>Từ 1.000.000 - 1.500.000</option>
-                    <option value="1500000-2000000" <?php echo isset($_POST['gia'])&&$_POST['gia']=="1500000-2000000" ?"selected":"";?>>Từ 1.500.000 - 2.000.000</option>
-                    <option value="2000000" <?php echo isset($_POST['gia'])&&$_POST['gia']=="2000000" ?"selected":"";?>>Trên 2.000.000</option>
+                    <option value="500000-1000000" <?php echo isset($_POST['gia']) && $_POST['gia'] == "500000-1000000" ? "selected" : ""; ?>>Từ 500.000 - 1.000.000</option>
+                    <option value="1000000-1500000" <?php echo isset($_POST['gia']) && $_POST['gia'] == "1000000-1500000" ? "selected" : ""; ?>>Từ 1.000.000 - 1.500.000</option>
+                    <option value="1500000-2000000" <?php echo isset($_POST['gia']) && $_POST['gia'] == "1500000-2000000" ? "selected" : ""; ?>>Từ 1.500.000 - 2.000.000</option>
+                    <option value="2000000" <?php echo isset($_POST['gia']) && $_POST['gia'] == "2000000" ? "selected" : ""; ?>>Trên 2.000.000</option>
                 </select>
                 <select class="form-control" name="size">
                     <option value="0">Size</option>
-                    <option value="49" <?php echo isset($_POST['size'])&&$_POST['size']== 49 ?"selected":"";?>>Size :49cm</option>
-                    <option value="50" <?php echo isset($_POST['size'])&&$_POST['size']== 50 ?"selected":"";?>>Size :50cm</option>
-                    <option value="51" <?php echo isset($_POST['size'])&&$_POST['size']== 51 ?"selected":"";?>>Size :51cm</option>
+                    <option value="49" <?php echo isset($_POST['size']) && $_POST['size'] == 49 ? "selected" : ""; ?>>Size :49cm</option>
+                    <option value="50" <?php echo isset($_POST['size']) && $_POST['size'] == 50 ? "selected" : ""; ?>>Size :50cm</option>
+                    <option value="51" <?php echo isset($_POST['size']) && $_POST['size'] == 51 ? "selected" : ""; ?>>Size :51cm</option>
                 </select>
-                <button type="submit" class=" w-full py-1 text-center bg-black text-white button_slide slide_right" name ="search">
+                <button type="submit" class=" w-full py-1 text-center bg-black text-white button_slide slide_right" name="search">
                     Tìm Kiếm
-                        </button>
+                </button>
             </form>
         </div>
         <!-- sản phẩm -->
         <div class="grid grid-cols-4 gap-4 my-8 ">
             <?php foreach ($products as $product) : ?>
-                <div class="">
+                <div class="relative">
                     <a href="http://localhost/WEB17301/Du_an_1/views/index.php?act=chitietsanpham&id=<?php echo $product['productId'] ?>" class="">
-                        <img class="sm:h-64 md:h-96" src="../img/<?php
+                        <img class="sm:h-64 md:h-96 absolute inset-0 opacity-100 hover:opacity-0 duration-300" src="../img/<?php
                                                                     $arr = explode(",", $product['productImage']);
                                                                     echo $arr[0];
+                                                                    ?>" alt="">
+                        <img class="sm:h-64 md:h-96" src="../img/<?php
+                                                                    $arr = explode(",", $product['productImage']);
+                                                                    echo $arr[1];
                                                                     ?>" alt="">
                         <div class="">
                             <a href="" class="text-base font-semibold"><?php echo $product['productName'] ?></a>
@@ -72,17 +76,19 @@
     #child2 {
         display: none;
     }
+
     .button_slide {
-            color: #FFF;
-            display: inline-block;
-            cursor: pointer;
-            box-shadow: inset 0 0 0 0 #faf5f8;
-            -webkit-transition: ease-out 0.4s;
-            -moz-transition: ease-out 0.4s;
-            transition: ease-out 0.4s;
-        }
-        .slide_right:hover {
-            box-shadow: inset 800px 0 0 0 #faf5f8;
-            color: black;
-        }
+        color: #FFF;
+        display: inline-block;
+        cursor: pointer;
+        box-shadow: inset 0 0 0 0 #faf5f8;
+        -webkit-transition: ease-out 0.4s;
+        -moz-transition: ease-out 0.4s;
+        transition: ease-out 0.4s;
+    }
+
+    .slide_right:hover {
+        box-shadow: inset 800px 0 0 0 #faf5f8;
+        color: black;
+    }
 </style>
